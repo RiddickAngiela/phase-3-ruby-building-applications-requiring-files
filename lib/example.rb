433f2d9f6a7821767cddd8e54a3bfd140ec3e96c
Riddick <riddick.angiela@student.moringaschool.com>
2022-10-26 22:25:03 +0300

    Completed assignment

--- lib/example.rb
+++ lib/example.rb
@@ -20,19 +20,20 @@ class Plant
   def initialize(name:)
     @name = name
     @@all << self
-  end
+     end
 
   def self.all
     @@all
   end
 end
 
-lawn = Garden.new(name: 'Front Lawn')
+lawn = Garden.new(name: 'Front Lawn') # we can call Garden.new because Garden is defined above
 
-basil = Plant.new(name: 'Basil')
+basil = Plant.new(name: 'Basil') # we can call Plant.new because Plant is defined above
 basil.garden = lawn
 
 cucumber = Plant.new(name: 'Cucumber')
-cucumber.garden = lawn
+cucumber.garden = lawn # we can associate a plant and a garden because both are defined above
 
 p lawn.plants
+# => [#<Plant:0x00007fa4440ab0c8 @name="Basil", @garden=#<Garden:0x00007fa4440997b0 @name="Front Lawn">>, #<Plant:0x00007fa4440b8bd8 @name="Cucumber", @garden=#<Garden:0x00007fa4440997b0 @name="Front Lawn">>]
